f3370789b5c0985b14220aac31999ca9a4dc0694
dur5t <mukasor@gmail.com>
2020-06-01 20:52:26 +0300

    Initial Commit

---
+++ IpLocationFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IpLocationFinder
+//
+//  Created by Алексей Чигарских on 01.06.2020.
+//  Copyright © 2020 Алексей Чигарских. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
