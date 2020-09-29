a940ecfe6a293482e428f36d85e8563afb9543e5
Aleksey Chigarskikh <surfspotdevelop@gmail.com>
2020-09-29 08:29:10 +0300

    1.2 refactoring

--- Extensions/Extensions.swift
+++ Extensions/Extensions.swift
@@ -10,19 +10,19 @@ import Foundation
 import UIKit
 
 extension UIViewController {
-
-  func showAlert(title: String, message: String, callback: @escaping () -> ()) {
-    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-     alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {
-       alertAction in
-       callback()
-     }))
-     self.present(alert, animated: true, completion: nil)
-   }
-
-   
+    
+    func showAlert(title: String, message: String, callback: @escaping () -> ()) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: {
+            alertAction in
+            callback()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
 
-    
-    
+
+
